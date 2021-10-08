33ca42298632381c00147ec8c9e3ed282c318cd4
kais manai <kaismanai1975@gmail.com>
2021-10-08 11:10:25 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../src/components/header/Header';
+import About from'../src/components/about/about';
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="App">
+     
+      <Header /> 
+      <About />
+
+    </div>
+  );
+}
+
+export default App;
+
